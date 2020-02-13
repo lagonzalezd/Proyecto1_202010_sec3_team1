136a03b6700b80b023275224c7681009cbcfe7f9
lagonzalezd <la.gonzalezd@uniandes.edu.co>
2020-02-13 07:31:42 -0500

    Controller

--- src/model/logic/Comparendo.java
+++ src/model/logic/Comparendo.java
@@ -1,4 +1,40 @@
 package model.logic;
 
+
+import java.util.Date;
+
 public class Comparendo {
+    private int objectId;
+    private Date fecha_hora;
+    private String des_infrac;
+    private String medio_dete;
+    private String clase_vehi;
+    private String tipo_servi;
+    private String infraccion;
+    private String localidad;
+
+    private double latitud;
+    private double longitud;
+
+    public Comparendo(int objeId, Date fecha, String descripcion, String detencion, String claseVeh, String tipoSer, String codInfraccion, String localidadP, double lonP, double latP)
+    {
+        objectId = objeId;
+        fecha_hora = fecha;
+        des_infrac = descripcion;
+        medio_dete = detencion;
+        clase_vehi = claseVeh;
+        tipo_servi = tipoSer;
+        infraccion = codInfraccion;
+        localidad = localidadP;
+        longitud = lonP;
+        latitud = latP;
+    }
+
+    public String toString() {
+        return "Comparendo [OBJECTID=" + objectId + ", FECHA_HORA=" + fecha_hora + ", DES_INFRAC=" + des_infrac
+                + ", MEDIO_DETE=" + medio_dete + ", CLASE_VEHI=" + clase_vehi + ", TIPO_SERVI=" + tipo_servi
+                + ", INFRACCION=" + infraccion + ", LOCALIDAD=" + localidad + ", latitud=" + latitud + ", longitud="
+                + longitud + "]";
+    }
+
 }
--- src/model/logic/Modelo.java
+++ src/model/logic/Modelo.java
@@ -13,60 +13,59 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.stream.JsonReader;
 
-import model.Comparendo;
+import model.logic.Comparendo;
+import model.data_structures.Stack;
 
 /**
  * Definicion del modelo del mundo
- *
  */
 public class Modelo {
 
-	public static String PATH = "./data/comparendos_dei_2018_small.geojson";
+    public static String PATH = "./data/comparendos_dei_2018_small.geojson";
 //	public static String PATH = "./data/comparendos_dei_2018.geojson";
 
 
-	public List<Comparendo> cargarDatos() {
+    public Stack cargarDatos() {
 
-		//TODO Cambiar la clase del contenedor de datos por la Estructura de Datos propia adecuada para resolver el requerimiento
-		List<Comparendo> datos = new ArrayList<Comparendo>();
+        Stack datos = new Stack();
 
-		JsonReader reader;
-		try {
-			reader = new JsonReader(new FileReader(PATH));
-			JsonElement elem = JsonParser.parseReader(reader);
-			JsonArray e2 = elem.getAsJsonObject().get("features").getAsJsonArray();
+        JsonReader reader;
+        try {
+            reader = new JsonReader(new FileReader(PATH));
+            JsonElement elem = JsonParser.parseReader(reader);
+            JsonArray e2 = elem.getAsJsonObject().get("features").getAsJsonArray();
 
 
-			SimpleDateFormat parser=new SimpleDateFormat("yyyy/MM/dd");
+            SimpleDateFormat parser = new SimpleDateFormat("yyyy/MM/dd");
 
-			for(JsonElement e: e2) {
-				int OBJECTID = e.getAsJsonObject().get("properties").getAsJsonObject().get("OBJECTID").getAsInt();
+            for (JsonElement e : e2) {
+                int OBJECTID = e.getAsJsonObject().get("properties").getAsJsonObject().get("OBJECTID").getAsInt();
 
-				String s = e.getAsJsonObject().get("properties").getAsJsonObject().get("FECHA_HORA").getAsString();
-				Date FECHA_HORA = parser.parse(s);
+                String s = e.getAsJsonObject().get("properties").getAsJsonObject().get("FECHA_HORA").getAsString();
+                Date FECHA_HORA = parser.parse(s);
 
-				String MEDIO_DETE = e.getAsJsonObject().get("properties").getAsJsonObject().get("MEDIO_DETE").getAsString();
-				String CLASE_VEHI = e.getAsJsonObject().get("properties").getAsJsonObject().get("CLASE_VEHI").getAsString();
-				String TIPO_SERVI = e.getAsJsonObject().get("properties").getAsJsonObject().get("TIPO_SERVI").getAsString();
-				String INFRACCION = e.getAsJsonObject().get("properties").getAsJsonObject().get("INFRACCION").getAsString();
-				String DES_INFRAC = e.getAsJsonObject().get("properties").getAsJsonObject().get("DES_INFRAC").getAsString();
-				String LOCALIDAD = e.getAsJsonObject().get("properties").getAsJsonObject().get("LOCALIDAD").getAsString();
+                String MEDIO_DETE = e.getAsJsonObject().get("properties").getAsJsonObject().get("MEDIO_DETE").getAsString();
+                String CLASE_VEHI = e.getAsJsonObject().get("properties").getAsJsonObject().get("CLASE_VEHI").getAsString();
+                String TIPO_SERVI = e.getAsJsonObject().get("properties").getAsJsonObject().get("TIPO_SERVI").getAsString();
+                String INFRACCION = e.getAsJsonObject().get("properties").getAsJsonObject().get("INFRACCION").getAsString();
+                String DES_INFRAC = e.getAsJsonObject().get("properties").getAsJsonObject().get("DES_INFRAC").getAsString();
+                String LOCALIDAD = e.getAsJsonObject().get("properties").getAsJsonObject().get("LOCALIDAD").getAsString();
 
-				double longitud = e.getAsJsonObject().get("geometry").getAsJsonObject().get("coordinates").getAsJsonArray()
-						.get(0).getAsDouble();
+                double longitud = e.getAsJsonObject().get("geometry").getAsJsonObject().get("coordinates").getAsJsonArray()
+                        .get(0).getAsDouble();
 
-				double latitud = e.getAsJsonObject().get("geometry").getAsJsonObject().get("coordinates").getAsJsonArray()
-						.get(1).getAsDouble();
+                double latitud = e.getAsJsonObject().get("geometry").getAsJsonObject().get("coordinates").getAsJsonArray()
+                        .get(1).getAsDouble();
 
-				Comparendo c = new Comparendo(OBJECTID, FECHA_HORA, DES_INFRAC, MEDIO_DETE, CLASE_VEHI, TIPO_SERVI, INFRACCION, LOCALIDAD, longitud, latitud);
-				datos.add(c);
-			}
+                Comparendo c = new Comparendo(OBJECTID, FECHA_HORA, DES_INFRAC, MEDIO_DETE, CLASE_VEHI, TIPO_SERVI, INFRACCION, LOCALIDAD, longitud, latitud);
+                datos.push((Comparable) c);
+            }
 
-		} catch (FileNotFoundException | ParseException e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-		}
-		return datos;
+        } catch (FileNotFoundException | ParseException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return datos;
 
-	}
+    }
 }
